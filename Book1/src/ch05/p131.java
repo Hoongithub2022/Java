04a20d98fa4811119b60609b2bd80d702d413e40
gns2506 <gns2506@gmail.com>
2022-08-23 10:53:47 +0900

    Java 실습

---
+++ Book1/src/ch05/p131.java
@@ -0,0 +1,10 @@
+package ch05;
+
+public class p131 {
+	String name;
+	int height;
+	double weight;
+	char gender;
+	boolean married;
+
+}
---
+++ Book1/src/ch05/p132.java
@@ -0,0 +1,12 @@
+package ch05;
+
+public class p132 {
+	int studentId;
+	String studentName;
+	int grade;
+	String address;
+	
+	public void showStudentInfo() {
+		System.out.println(studentName + "," + address);
+	}
+}
