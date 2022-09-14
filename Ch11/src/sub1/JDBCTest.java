9def07569bb9a930d3728841994134764e7ec59a
gns2506 <gns2506@gmail.com>
2022-09-14 10:46:14 +0900

    2022/09/14 JAVA 수업

--- Ch11/src/sub1/JDBCTest.java
+++ Ch11/src/sub1/JDBCTest.java
@@ -10,24 +10,24 @@ import java.sql.DriverManager;
  */
 public class JDBCTest {
 	public static void main(String[] args) {
-		
+
 		String host = "jdbc:mysql://127.0.0.1:3306/java1db";
 		String user = "root";
 		String pass = "1234";
-		
+
 		try {
-		// 1단계 - JDBC 드라이버 로드
-		Class.forName("com.mysql.cj.jdbc.Driver");
-	
-		// 2단계 - 데이터베이스 접속
-		Connection conn = DriverManager.getConnection(host, user, pass);
-		
-			if(conn != null) {
+			// 1단계 - JDBC 드라이버 로드
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// 2단계 - 데이터베이스 접속
+			Connection conn = DriverManager.getConnection(host, user, pass);
+
+			if (conn != null) {
 				System.out.println("데이터베이스 접속 성공!");
-			}else {
+			} else {
 				System.out.println("데이터베이스 접속 실패!");
 			}
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println("접속 테스트 완료...");
